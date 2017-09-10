429e1783b5997fd1eb8cc45fe405838fb583aabb
Giacomo Carnovalini <giacomo.carnovalini@gmail.com>
2017-09-10 14:37:06 +0100

    screen outputs inputs on button press - no calculations yet

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 /* jshint esversion: 6 */
 
 const app = new Vue({
-
+  el: '#app',
+  data: {
+    current: '0',
+    concat: '0',
+  },
+  methods: {
+    clearAll: function() {
+      this.current = 0;
+      this.concat = 0;
+    },
+    clearEntry: function() {
+      var len = this.current.length;
+      var entryCleared = this.concat.substring(0, this.concat.length-len);
+      this.concat = entryCleared;
+      this.current = '0';
+    },
+    typeNumber: function(n) {
+      var num = n.toString();
+      if (this.current == '*' || this.current == '/' || this.current == '+' || this.current == '-') {
+        this.current = num;
+        this.concat += num;
+      } else if (this.concat != 0 && this.concat.length > 0 ) {
+        if (this.current == 0) {
+          this.current = num;
+          this.concat += num;
+        } else {
+          this.current += num;
+          this.concat += num;
+        }
+      } else {
+        this.current = num;
+        this.concat = num;
+      }
+    },
+    typeOperator: function(op) {
+      var operator = op.toString();
+      this.current = operator;
+      this.concat += operator;
+    }
+  }
 });
+// TO DO: decimal & equal buttons
+// TO DO: actual calculation!
