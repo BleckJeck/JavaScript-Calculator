9164679a65d8fa5c9c4c89336b42ad81e9648a38
Giacomo Carnovalini <giacomo.carnovalini@gmail.com>
2017-09-10 18:03:34 +0100

    added calculations

--- app.js
+++ app.js
@@ -5,42 +5,80 @@ const app = new Vue({
   data: {
     current: '0',
     concat: '0',
+    total: 0,
+    ceAllow: true,
+    pointAllow: true,
+    printTotal: false,
   },
   methods: {
     clearAll: function() {
-      this.current = 0;
-      this.concat = 0;
+      this.current = '0';
+      this.concat = '0';
+      this.ceAllow = true;
+      this.pointAllow = true;
+      this.printTotal = false;
     },
     clearEntry: function() {
       var len = this.current.length;
       var entryCleared = this.concat.substring(0, this.concat.length-len);
-      this.concat = entryCleared;
-      this.current = '0';
+      if (this.ceAllow && !this.printTotal) {
+        this.concat = entryCleared;
+        this.current = '0';
+        this.ceAllow = false;
+        this.pointAllow = true;
+      }
     },
     typeNumber: function(n) {
       var num = n.toString();
       if (this.current == '*' || this.current == '/' || this.current == '+' || this.current == '-') {
         this.current = num;
         this.concat += num;
-      } else if (this.concat != 0 && this.concat.length > 0 ) {
-        if (this.current == 0) {
+        this.ceAllow = true;
+      } else if (this.concat != '0' && this.concat.length > 0 ) {
+        if (this.current === '0') {
           this.current = num;
           this.concat += num;
+          this.ceAllow = true;
         } else {
           this.current += num;
           this.concat += num;
+          this.ceAllow = true;
         }
       } else {
         this.current = num;
         this.concat = num;
+        this.ceAllow = true;
+      }
+    },
+    typePoint: function() {
+      if (this.pointAllow) {
+        if (this.current == '*' || this.current == '/' || this.current == '+' || this.current == '-') {
+          this.current = "0.";
+          this.concat += "0.";
+          this.ceAllow = true;
+          this.pointAllow = false;
+        } else {
+          this.current += ".";
+          this.concat += ".";
+          this.ceAllow = true;
+          this.pointAllow = false;
+        }
       }
     },
     typeOperator: function(op) {
       var operator = op.toString();
-      this.current = operator;
-      this.concat += operator;
+      if (this.concat.substr(-1) != '*' && this.concat.substr(-1) != '/' && this.concat.substr(-1) != '+' && this.concat.substr(-1) != '-') {
+        this.current = operator;
+        this.concat += operator;
+        this.ceAllow = true;
+        this.pointAllow = true;
+      }
+    },
+    calculate: function() {
+      this.total = eval(this.concat);
+      this.current = (Math.round(this.total * 1000)/1000).toString();
+      this.concat += '=' + (Math.round(this.total * 1000)/1000).toString();
+      this.printTotal = true;
     }
   }
 });
-// TO DO: decimal & equal buttons
-// TO DO: actual calculation!
